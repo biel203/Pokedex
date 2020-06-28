7afd355814b06b5b69b5ef8ef8c632f02edda4ab
Gabriel Stein <gabriel.g.stein@gmail.com>
2020-06-28 10:15:49 -0300

    Tabs component

---
+++ components/base/Tabs.tsx
@@ -0,0 +1,110 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const TabContent = styled.div`
+  flex: 1;
+  width: 100%;
+`;
+
+const TabPanel = ({ children }) => (
+  <TabContent role="tabpanel" tabindex="0">
+    {children}
+  </TabContent>
+);
+
+interface Props {
+  tabBreak?: string;
+}
+
+interface State {
+  children?: any;
+  tabBreak?: string;
+  selectedTab: number;
+}
+
+class Tabs extends Component<Props, State> {
+  static Panel = TabPanel;
+
+  state: State = {
+    selectedTab: 0,
+    tabBreak: "768px",
+  };
+
+  selectTab = (tabIndex) => {
+    this.setState({ selectedTab: tabIndex });
+  };
+
+  render() {
+    const { children, tabBreak } = this.props;
+    const { selectedTab } = this.state;
+
+    return (
+      <TabsWrapper>
+        <TabList breakPoint={tabBreak} role="tablist">
+          {React.Children.map(children, ({ props: { label } }, index) => (
+            <TabButton
+              role="tab"
+              selected={selectedTab === index}
+              aria-selected={selectedTab === index ? "true" : "false"}
+              onClick={() => this.selectTab(index)}
+            >
+              {label}
+            </TabButton>
+          ))}
+        </TabList>
+
+        <Content>
+          {React.Children.map(children, (comp, index) =>
+            selectedTab === index ? comp : undefined
+          )}
+        </Content>
+      </TabsWrapper>
+    );
+  }
+}
+
+const TabsWrapper = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+`;
+
+const TabButton = styled.button`
+  flex: 1;
+  color: white;
+  height: 50px;
+  padding: 0px 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 16px;
+  cursor: default;
+  background: transparent;
+  outline: none;
+  transition: border-color 0.2s ease-in;
+  border: none;
+`;
+
+const TabList = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  @media (max-width: ${(props) => props.breakPoint}) {
+    flex-direction: column;
+    & > div,
+    & > div > button {
+      width: 100%;
+    }
+  }
+`;
+
+const Content = styled.div`
+  flex: 1;
+  width: 100%;
+  padding-top: 16px;
+  border-radius: 25px;
+  padding: 28px;
+  background: #e5e5e5;
+`;
+
+export default Tabs;
